b4ff538890e8f69ad5d2567126f89541ff0b4a8c
clark_b <bishop.clark@burnaby.ca>
2017-09-27 09:29:28 -0700

    format fixups to smb.conf and krb5.conf

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,10 +7,9 @@
 # cheat around the lack of crypto dbags
 include_recipe 'all-datacenter-attributes::realm'	# AD centralized settings
 
-package %w(PackageKit)
-package %w(samba samba-client samba-common samba-winbind samba-winbind-clients oddjob-mkhomedir dbus pam_krb5)
-package %w(krb5-workstation)
-package %w(adcli)
+package %w(PackageKit samba samba-client samba-common samba-winbind
+	samba-winbind-clients oddjob-mkhomedir dbus pam_krb5 krb5-workstation
+	adcli)
 
 case node[:realm][:auth]||''
 when 'sssd'
@@ -23,12 +22,27 @@ else
     action [ :enable, :start ]
   end
 
-  execute "authconfig  --enablewinbindoffline --disablecache --enablewinbind  --enablewinbindauth --smbsecurity=ads --smbworkgroup=TEST --smbrealm=#{node[:realm][:directory_name].upcase} --smbservers=#{node[:realm][:servers].join(',')} --enablewinbindusedefaultdomain --winbindtemplatehomedir=/home/%U  --winbindtemplateshell=/bin/bash --krb5realm=#{node[:realm][:directory_name].upcase} --disablekrb5kdcdns --disablekrb5realmdns --enablelocauthorize  --enablemkhomedir --enablepamaccess  --updateall > /etc/.authconfig" do
+  execute "authconfig" do
+    command "authconfig " +
+      "--enablewinbind --enablewinbindauth --enablewinbindoffline --disablecache " +
+      "--enablewinbindusedefaultdomain " +
+      "--smbsecurity=ads --smbworkgroup=#{node[:realm][:netbios]} " +
+      "--smbrealm=#{node[:realm][:directory_name].upcase} " +
+      "--smbservers=#{node[:realm][:servers].join(',')} " +
+      "--winbindtemplatehomedir=/home/%U --winbindtemplateshell=/bin/bash " +
+      "--krb5realm=#{node[:realm][:directory_name].upcase} " +
+      "--disablekrb5kdcdns --disablekrb5realmdns " +
+      "--enablelocauthorize --enablepamaccess  " +
+      "--enablemkhomedir --updateall > /etc/.authconfig"
     creates '/etc/.authconfig'
   end
 
-# yes, still need to patch up the krb5.conf to add in the KDC addresses  grr....
-
+  # yes, we still need to patch up the krb5.conf to add in the KDC
+  # addresses because authconfig is weak.  grr....
+  template '/etc/samba/smb.conf'
+  template '/etc/security/pam_winbind.conf'
+  template '/etc/krb5.conf'
+  
   %w(messagebus oddjobd).each do |svc|
     service svc do 
       action [ :enable, :start ]
@@ -41,10 +55,6 @@ else
     end
   end
 
-  template '/etc/samba/smb.conf'
-  template '/etc/security/pam_winbind.conf'
-  template '/etc/krb5.conf'
-  
   execute 'join ad in winbind' do
     command "net ads join -U #{node.run_state['realm_username']}%#{node.run_state['realm_password']} " +
       "-S #{node[:realm][:servers][0]}"
@@ -53,18 +63,27 @@ else
     not_if " [ -f '/etc/krb5.keytab' ] && id #{node[:fqdn].split('.')[0]}$"
   end
 
-  Chef::Log.warn "This can sometimes fail due to AD caching and refresh inadequacies.  Run it again."
+  # this can sometimes fail.  The only solution is to wait for a synch
+  # between AD servers, and then try again.
   (node[:realm][:secgroups]||[]).each do |grp|
     execute "add #{node[:fqdn]} to group #{grp}@#{node[:realm][:realm_name]}" do
       sensitive	true
-      command "adcli add-member -D #{node[:realm][:realm_name]} -U #{node.run_state['realm_secname']} #{grp} #{node[:fqdn].split('.')[0]}$ <<< '#{node.run_state['realm_secpass']}'"
+      command "adcli add-member " + 
+        "-D #{node[:realm][:realm_name]} " + 
+        "-S #{node[:realm][:servers][0]} " + 
+        "-U #{node.run_state['realm_secname']} " + 
+        "#{grp} #{node[:fqdn].split('.')[0]}$ " + 
+        "<<< '#{node.run_state['realm_secpass']}'"
       only_if "id #{node[:fqdn].split('.')[0]}$|grep -vqi '(#{grp})'"
     end   	# execute
   end		# each
 
   (node[:realm][:srvtix]||[]).map(&:upcase).each do |srv|
     execute "get service ticket #{srv}/#{node[:fqdn]}@#{node[:realm][:realm_name].upcase}" do
-      command "net ads keytab add #{srv} -U #{node.run_state['realm_username']}%#{node.run_state['realm_password']}; klist -k"
+      command "net ads keytab " + 
+        "add #{srv} " + 
+        "-U #{node.run_state['realm_username']}%#{node.run_state['realm_password']}" + 
+        "; klist -k"
       not_if "klist -k | grep -qi '#{srv}/#{node[:fqdn]}@#{node[:realm][:realm_name].upcase}'"
     end
   end
